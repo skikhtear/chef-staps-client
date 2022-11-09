704f9102c9557d2b30809f253775c8a23fcb83b8
skikhtear <skikhtear@gmail.com>
2022-11-09 21:22:59 +0600

    review

--- src/component/Page/Home/Services/ServiceDetails.js
+++ src/component/Page/Home/Services/ServiceDetails.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import PostReview from '../../Review/PostReview';
 
 const ServiceDetails = () => {
     const service = useLoaderData();
     const {_id,name,picture,price,detail } = service;
     return (
-        <div className="card w-full bg-base-100 shadow-xl">
-            <figure><img src={picture} alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    {name}
-                    <div className="badge badge-secondary">NEW</div>
-                </h2>
-                <p>{detail}</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Price</div>
-                    <div className="badge badge-outline">${price}</div>
+        <div>
+            <div className="card w-full bg-base-100 shadow-xl">
+                <figure><img src={picture} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        {name}
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>{detail}</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">Price</div>
+                        <div className="badge badge-outline">${price}</div>
+                    </div>
                 </div>
             </div>
+            <PostReview></PostReview>
         </div>
     );
 };
---
+++ src/component/Page/Review/PostReview.js
@@ -0,0 +1,81 @@
+import React, { useContext } from 'react';
+import { Image } from 'react-bootstrap';
+import { FaUser } from 'react-icons/fa';
+import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../../Contexts/AuthProvider';
+
+
+
+const PostReview = () => {
+    const { _id,title  } = useLoaderData();
+    const { user } = useContext(AuthContext);
+
+    const handlePlaceReview = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.Name.value;
+        const email = user?.email || 'unregistered';
+        const message = form.message.value;
+        const UserImg = user?.photoURL || <FaUser></FaUser>;
+
+        const review = {
+            service: _id,
+            customer: name,
+            email,
+            message,
+            UserImg
+        }
+
+        // if(phone.length > 10){
+        //     alert('Phone number should be 10 characters or longer')
+        // }
+        // else{
+
+        // }
+
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                // authorization: `Bearer ${localStorage.getItem('chef-steps')}`
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged) {
+                    alert('Post Review successfully')
+                    form.reset();
+
+                }
+            })
+            .catch(er => console.error(er));
+
+
+    }
+    return (
+        <div className='flex justify-center' >
+            <form onSubmit={handlePlaceReview}>
+                <h2 className="text-5xl font-bold ">Please Review {title}</h2>
+                <div className='flex justify-center my-11'>
+                    {user?.photoURL ?
+                        <img className='rounded-full' src={user?.photoURL} alt=""style={{ height: '70px' }} />
+                        : <FaUser></FaUser>
+                    }
+                </div>
+                <div className='w-3/1'>
+                    <input name="email" type="text" placeholder="Your email" defaultValue={user?.email} className="input input-ghost w-full mb-5 input-bordered" readOnly />
+                    <input name="Name" type="text" placeholder="Name" className="input input-ghost w-full mb-5  input-bordered" />
+                </div>
+                <textarea name="message" className="textarea textarea-bordered h-24 w-full" placeholder="Your Message" required></textarea>
+                <div className='flex justify-center mt-5'>
+                    <input className='btn' type="submit" value="Post Review" />
+                  </div>
+                 
+            </form>
+        </div>
+    );
+};
+
+export default PostReview;
