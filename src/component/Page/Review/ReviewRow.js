4227c63ab8ccafd6a9004c35eb2766628b79c56b
skikhtear <skikhtear@gmail.com>
2022-11-09 23:25:06 +0600

    review

--- src/component/Page/Review/ReviewRow.js
+++ src/component/Page/Review/ReviewRow.js
@@ -23,26 +23,20 @@ const ReviewRow = ({ order, handleDelete, handleStatusUpdate }) => {
                 <div className="flex items-center space-x-3">
                     <div className="avatar">
                         <div className="rounded w-24 h-24">
-                                <img src={UserImg} alt="Avatar Tailwind CSS Component" />
+                                <img src={UserImg} alt=""  className='rounded-full'/>
                         </div>
                     </div>
                     <div>
                         <div className="font-bold">{customer}</div>
-                        <div className="text-sm opacity-50">{serviceName}</div>
                     </div>
                 </div>
             </td>
             <td>
                 {serviceName}
                 <br />
-                <span className="badge badge-ghost badge-sm">${message}</span>
+                <span className="badge badge-ghost badge-sm">{message}</span>
             </td>
-            {/* <td>Purple</td>
-            <th>
-                <button
-                    onClick={() => handleStatusUpdate(_id)}
-                    className="btn btn-ghost btn-xs">{status ? status : 'pending'}</button>
-            </th> */}
+            
         </tr>
     );
 };
